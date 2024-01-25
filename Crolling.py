a11133fab39276f9441714cf260fec4af2e10ed3
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2024-01-25 22:30:31 +0900

    Create Crolling.py
    First

---
+++ Crolling.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://www.google.com/search?q= INPUTYOUR_KEY_WORDS ')
+driver.implicitly_wait(2)
+
+clicks = 10  # Set the number of questions you want to click
+
+for i in range(clicks):
+    # Fetch the list of questions again to get updated elements
+    paa_questions = driver.find_elements(By.CLASS_NAME, "related-question-pair")
+
+    if i < len(paa_questions):
+        print('Clicking question #', i + 1)
+        try:
+            # Click the question
+            paa_questions[i].click()
+            time.sleep(2)  # Wait for the content to load
+
+            # Fetch the list of questions again to get the updated question text
+            updated_questions = driver.find_elements(By.CLASS_NAME, "CSkcDe")
+            if i < len(updated_questions):
+                question_text = updated_questions[i].text
+                print("Question:", question_text)
+            else:
+                print("No updated question found for index", i)
+
+        except Exception as e:
+            print("Error clicking a question or extracting text:", e)
+    else:
+        print("No more questions available at index", i)
+
+driver.quit()
